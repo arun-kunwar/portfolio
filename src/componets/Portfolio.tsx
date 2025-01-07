ddfaefc637f9de5173057d4c37449682722b7390
Arun kunwar <arunkunwar981.com>
2025-01-07 21:22:13 +0545

    complete client reviews

--- src/componets/Portfolio.tsx
+++ src/componets/Portfolio.tsx
@@ -1,40 +1,41 @@
 function Portfolio() {
   return (
-    <div className="bg-white py-5 px-6">
-      <div className="max-w-7xl mx-auto  ">
-        <div>
-          <h2 className=" text-4xl font-bold pl-3 border-l-4 border-l-red-500">
-            {" "}
-            Client Reviews
+    <section className="bg-white py-10 px-6">
+      <div className="max-w-7xl mx-auto">
+       
+        <div className="mb-10">
+          <h2 className="text-5xl font-bold pl-3 border-l-4 border-red-500">
+            Client <br /> Reviews
           </h2>
         </div>
-        <div className="max-w-[250px] md:max-w-3xl  gap-10  flex flex-col items-center md:flex md:flex-row">
-        <div className=" leading-relaxed">
-          <h2>Arun</h2>
-          <p>
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-            Perferendis, consectetur enim unde illo aperiam deleniti autem
-            maxime quasi!
-          </p>
-        </div>
-       
-        <div className=" lead">
-          <h1>Arun</h1>
-          <p>
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Illo odio
-            ex id excepturi. Nisi exercitationem modi quam totam.{" "}
-          </p>
-        </div>
-        <div className="">
-          <h1>Arun</h1>
-          <p>
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ipsam,
-            deleniti nisi! Pariatur, recusandae sit odit nam officia repellat.
-          </p>
-        </div>
+
+    
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
+          <article className="p-6 bg-gray-50 shadow-lg rounded-lg leading-relaxed">
+            <h3 className="font-semibold text-lg mb-2">Arun</h3>
+            <p>
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+              Perferendis, consectetur enim unde illo aperiam deleniti autem
+              maxime!
+            </p>
+          </article>
+          <article className="p-6 bg-gray-50 shadow-lg rounded-lg leading-relaxed">
+            <h3 className="font-semibold text-lg mb-2">Arun</h3>
+            <p>
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Illo
+              odio ex id excepturi. Nisi exercitationem modi quam totam.
+            </p>
+          </article>
+          <article className="p-6 bg-gray-50 shadow-lg rounded-lg leading-relaxed">
+            <h3 className="font-semibold text-lg mb-2">Arun</h3>
+            <p>
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ipsam,
+              deleniti nisi! Pariatur, recusandae sit odit nam officia repellat.
+            </p>
+          </article>
         </div>
       </div>
-    </div>
+    </section>
   );
 }
 
